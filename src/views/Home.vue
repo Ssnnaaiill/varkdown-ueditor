d1b239e40182404cff95f6677e720a48c2d52eb9
Ssnnaaiill <newdk3025@gmail.com>
2019-11-20 09:52:16 +0900

    🎉 Initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="wrapper">
+    <!-- Components -->
+  </div>
+</template>
+
+<script>
+export default {
+  components: {}
+};
+</script>
+
+<style lang="scss">
+.wrapper {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+}
+</style>
